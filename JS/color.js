50c4118e080071316fd1de5b1079d7f113118197
tridoz <tridoz2905@gmail.com>
2024-10-30 13:08:52 +0100

    aggiustato colori porco dio cane finalmente

--- JS/color.js
+++ JS/color.js
@@ -1,16 +1,33 @@
-const menu_items_color = document.querySelectorAll('.menu_item');
 const page_titles = document.querySelectorAll('.arg_title');
+const menu_items_color = document.querySelectorAll('.menu_item');
+const numero = document.querySelector('.page_number');
 
 function setCardColour(){
+
+    const classi = numero.classList;
+
+    let numeroIntero = null;
+    for (let classe of classi) {
+        if (classe.startsWith('_')) {
+            numeroIntero = parseInt(classe.substring(1), 10);
+            break;
+        }
+    }
+
+
     let j = 0;
     let increment = 360 / menu_items_color.length;
 
     for(let i = 0 ; i<menu_items_color.length ; i++){
         menu_items_color[i].setAttribute('style', `filter: hue-rotate(${j}deg);`); 
         
-        for( let k = 3*i ; k< ( (i+1)*3) ; k++){
-            page_titles[k].setAttribute('style', `filter: hue-rotate(${j}deg);`); 
+        if( i == numeroIntero){
+            for( let k = 0 ;  k< 3 ; k++){
+                page_titles[k].setAttribute('style', `filter: hue-rotate(${j}deg);`); 
+            }
         }
+
+        
 
         j += increment; 
     }
