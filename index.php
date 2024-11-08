0017072fb43082c284c0706169b55b11df488774
tridoz <tridoz2905@gmail.com>
2024-11-08 10:49:52 +0100

    prova php pagina iniziale con php esterno

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+
+    <head>
+        <title>Gestore Impresa</title>
+		<link rel="stylesheet" href="CSS/style.css" />
+		
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		   
+    </head>
+
+    <body>
+
+        <div class="container_bg">
+			<span class="title-up fade__in">GESTIONE IMPRESA</span>
+
+
+			<div class="carousel-container">
+			
+			
+				<div class="carousel">
+                    
+					<?php
+                        include "PHP_FUNCTIONS/cards,php";
+                        $content = create_cards();
+                        echo $content;
+                    ?>
+                
+                </div>
+
+			</div>
+	
+
+		</div>
+
+        </div>
+            <button class="btn prev-btn">&#10094;</button>
+            <button class="btn next-btn">&#10095;</button>
+        </div>
+
+        <div class="who_we_are">
+                Made by<br>
+                Concaro Davide - Popa Sebastiano - Tridapali Leonardo<br>
+                5BIIN 2024-2025<br>
+        </div>
+
+		<script src="JS/main.js"></script>
+
+
+    </body>
+
+
+    </html>
