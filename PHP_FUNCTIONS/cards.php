331463881dff140756de1a41c8030fd651777d04
tridoz <tridoz2905@gmail.com>
2024-11-11 16:04:17 +0100

    ritornata al default

--- PHP_FUNCTIONS/cards.php
+++ PHP_FUNCTIONS/cards.php
@@ -8,30 +8,21 @@ function create_cards($table_name) {
 
     foreach( $table_rows as $data){
 
-        $form = "";
-        if( $data["Title"] == "Our Projects"){
-            $form = "
-                <a href=\"{$data["Link"]}\"class=\"see-more\">SOURCE CODE</button>     
-            ";
-        }else{
-            $form = "
-                <form action=\"page.php\" method=\"POST\" >
-                    <input name=\"page_name\" value=\"{$data['Title']}\" type=\"hidden\">
-                    <button type=\"submit\" class=\"see-more\">MORE INFO</button>
-                </form>
-            ";
-        }
 
         $content .= "
             <div class=\"parent fade__in\">
                 <div class=\"card\">
                     <div class=\"content-box\">
                         <h1 class=\"card-title\">{$data['Title']}</h1>
+
                         <p class=\"card-content\">
                             {$data['Arg_Desc']}
                         </p>
 
-                        {$form}
+                        <form action=\"page.php\" method=\"POST\" >
+                            <input name=\"page_name\" value=\"{$data['Title']}\" type=\"hidden\">
+                            <button type=\"submit\" class=\"see-more\">MORE INFO</button>
+                        </form>
 
                     </div>
                     <div class=\"date-box\">
