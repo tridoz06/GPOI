d6f7dd6b38c219e297c3b5188b38be29d41c1932
tridoz <tridoz2905@gmail.com>
2024-11-13 21:38:51 +0100

    aggiunto autoscroll-y per projects.php e aggiornata page.php per far funzionare colegamento da navbar ad ourprojects.php

--- page.php
+++ page.php
@@ -18,13 +18,24 @@
                 <button class=\"prev-btn btn\">&#10094;</button>";
 
                 foreach( $table_rows_main_arg as $data){
-                    
-                    $nav_bar_content .= "
-                        <div class=\"menu_item\">
-                            <form action=\"page.php\" method=\"POST\">
+                    $form = "";
+                    if($data["Title"] == "Our Projects"){
+                        $form = "
+                            <form action=\"projects.php\" method=\"POST\">
+                                <button type=\"submit\" class=\"menu_item_link\">{$data["Title"]}</button>
+                            </form>
+                        ";
+                    }else{
+                        $form = "
+                             form action=\"page.php\" method=\"POST\">
                                 <input name=\"page_name\" value=\"{$data["Title"]}\" type=\"hidden\">
                                 <button type=\"submit\" class=\"menu_item_link\">{$data["Title"]}</button>
                             </form>
+                        ";
+                    }
+                    $nav_bar_content .= "
+                        <div class=\"menu_item\">
+                            {$form}
                         </div>
                         ";
                 }
