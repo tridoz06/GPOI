35a6e23718e96679e9558a8cab27bdef37f6cfd0
tridoz <tridoz2905@gmail.com>
2024-11-13 21:22:37 +0100

    leggero aggiornamento pagina progetti

--- projects.php
+++ projects.php
@@ -1,35 +1,37 @@
 <!DOCTYPE html>
 <html lang="it">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>I miei progetti su GitHub</title>
-    <link rel="stylesheet" href="CSS/projects.css">
-</head>
-<body>
-    <header>
-        <h1>I miei progetti su GitHub</h1>
-        <p>Esplora i miei ultimi lavori e contributi.</p>
-        <form action="index.php" method="POST">
-            <button class="btn">HOME</button>
-        </form>
-    </header>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>I nostri progetti GitHub</title>
+        <link rel="stylesheet" href="CSS/projects.css">
+    </head>
+    <body>
+        <header>
+            <h1>I nostri Progetti GitHub</h1>
+            <p>Esplora i nostri ultimi fantastici lavori.</p>
+            <form action="index.php" method="POST">
+                <button class="btn">HOME</button>
+            </form>
+        </header>
 
-    <section class="projects-grid">
+        <section class="projects-grid">
 
-        <?php
-            include "PHP_FUNCTIONS/projects_card.php";
+            <?php
+                include "PHP_FUNCTIONS/projects_card.php";
 
-            
-            $content = create_cards();
-            
-            echo $content;
+                
+                $content = create_cards();
+                
+                echo $content;
 
-        ?>
-    </section>
+            ?>
+        </section>
 
-    <footer>
-        <p>© 2024 Il tuo nome. Tutti i diritti riservati.</p>
-    </footer>
-</body>
+        <footer>
+            <p>© 2024 Concaro Davide - Popa Sebastiano - Tridapali Leonardo 5BIIN ITIS E. Fermi.<br>
+            Tutti i diritti riservati.</p>
+        </footer>
+
+    </body>
 </html>
