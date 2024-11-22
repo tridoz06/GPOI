4fe424991d5d63b9fa49925bad852b5f0b55eb05
tridoz <tridoz2905@gmail.com>
2024-11-22 17:38:00 +0100

    creata classe per "privacy" dati

--- PHP_FUNCTIONS/read_database.php
+++ PHP_FUNCTIONS/read_database.php
@@ -1,14 +1,20 @@
 <?php
+Class db_credentials{
+    public static $servername = "185.229.236.208";
+    public static $username = "sito";
+    public static $password = "Sito2024_";
+    public static $dbname = "DB_Sito";
+}
 
 function read( $query) {
 
-    $servername = "185.229.236.208";
-    $username = "sito";
-    $password = "Sito2024_";
-    $dbname = "DB_Sito";
+
+
 
     try{
-        $pdo = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
+        $dsn = "mysql:host=" . db_credentials::$servername . ";dbname=" . db_credentials::$dbname . ";charset=utf8";
+
+        $pdo = new PDO($dsn, db_credentials::$username, db_credentials::$password);
         $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     }catch(PDOException $e){
@@ -17,7 +23,6 @@ function read( $query) {
 
     $stmt = $pdo -> prepare($query );
     $stmt->execute();
-
 
     $table_rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
     return $table_rows;
